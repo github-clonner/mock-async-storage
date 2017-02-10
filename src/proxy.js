dd3ea6d642e74c7012b5595a7fffb4449a51a953
Metál Ádám <devmetal91@gmail.com>
2017-02-10 10:27:47 +0100

    :ambulance: eslint fixes

--- src/proxy.js
+++ src/proxy.js
@@ -1,38 +1,38 @@
 // @flow
 
-import AsyncStorageMock from './AsyncStorageMock';
+import AsyncStorageMock from './AsyncStorageMock'
 
 const returns: Map<string, any> = new Map()
 
 const handler = {
-  get(target: Object, propKey: string) {
-    const origin = target[propKey];
+  get (target: Object, propKey: string) {
+    const origin = target[propKey]
     if (typeof origin !== 'function') {
       return origin
     }
 
     return async function (...args) {
-      const ret = returns.get(propKey);
-      
+      const ret = returns.get(propKey)
+
       if (!ret) {
-        return origin.apply(this, args);
+        return origin.apply(this, args)
       }
 
-      const errback = args.find(a => typeof a === 'function');
+      const errback = args.find(a => typeof a === 'function')
       if (typeof ret === Error) {
         if (errback) {
-          errback(ret, null);
+          errback(ret, null)
         }
         throw ret
       } else {
         if (errback) {
-          errback(null, ret);
+          errback(null, ret)
         }
         return ret
       }
     }
   }
-};
+}
 
 const proxy: AsyncStorageMock = new Proxy(new AsyncStorageMock(), handler)
 
@@ -40,4 +40,4 @@ export const willThrow = (fn: string, err: Error) => returns.set(fn, err)
 
 export const willReturn = (fn: string, value: any) => returns.set(fn, value)
 
-export default proxy;
+export default proxy
